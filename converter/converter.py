d3ff4f8d77c3783df8e944e79beb677fc6c04157
ABHI-xd <94308307+ABHI-xd@users.noreply.github.com>
2021-11-14 22:13:23 +0530

    Create converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from helpers.errors import FFmpegReturnCodeError
+from Abhixd.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
