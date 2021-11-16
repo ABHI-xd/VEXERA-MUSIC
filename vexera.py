cc3b4a073cfd38abec73a371519b815cc76595e1
ABHI-xd <94308307+ABHI-xd@users.noreply.github.com>
2021-11-16 13:07:27 +0530

    Update vexera.py

--- vexera.py
+++ vexera.py
@@ -1,7 +1,7 @@
 import requests
 
 
-from callsmusic.callsmusic import run
+from callsmusic import run
 
 from pyrogram import Client as Bot
 from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
