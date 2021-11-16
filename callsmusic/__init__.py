c5bed2dffc13b4478f7d6361cef67459552d5371
ABHI-xd <94308307+ABHI-xd@users.noreply.github.com>
2021-11-16 11:44:01 +0530

    Update __init__.py

--- callsmusic/__init__.py
+++ callsmusic/__init__.py
@@ -1,6 +1,2 @@
-from pyrogram import Client
-
-import config
-
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
-run = client.run
+from .callsmusic import pytgcalls, run
+from . import queues
