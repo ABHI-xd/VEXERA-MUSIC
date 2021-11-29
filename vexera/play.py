2bb7374df254111fe634ccd1f6b86a6f7b861dbe
ABHI-xd <94308307+ABHI-xd@users.noreply.github.com>
2021-11-29 10:29:45 +0530

    Update play.py

--- vexera/play.py
+++ vexera/play.py
@@ -977,7 +977,7 @@ async def ytplay(_, message: Message):
     file_path = await convert(download(url))
     ACTV_CALLS = []
     for x in callsmusic.pytgcalls.active_calls:
-        ACTV_CALLS(int(x.chat_id))
+        ACTV_CALLS.append(int(x.chat_id))
     if int(message.chat.id) in ACTV_CALLS:
         position = await queues.put(chat_id, file=file_path)
         qeue = que.get(chat_id)
