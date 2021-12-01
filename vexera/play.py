e2263e9294ab90e8cf7d107f7802e94b86d09c9b
ABHI-xd <94308307+ABHI-xd@users.noreply.github.com>
2021-12-01 20:55:21 +0530

    Update play.py

--- vexera/play.py
+++ vexera/play.py
@@ -12,7 +12,7 @@ import aiohttp
 import yt_dlp
 from youtube_search import YoutubeSearch
 import converter
-from youtube import youtube
+from  import youtube
 from config import DURATION_LIMIT, que, SUDO_USERS
 from function.admins import admins as a
 from Abhixd.filters import command
